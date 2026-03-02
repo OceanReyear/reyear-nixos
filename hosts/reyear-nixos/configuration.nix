a62ed93868b84d20b24f43d75bc383f95d132004
reyear <reyearocean@qq.com>
2026-03-02 11:20:36 +0800

    end5

--- hosts/reyear-nixos/configuration.nix
+++ hosts/reyear-nixos/configuration.nix
@@ -138,6 +138,18 @@ in {
   system.stateVersion = "25.11";
   networking.hostName = "reyear-nixos";
   time.timeZone = "Asia/Shanghai";
+  i18n.defaultLocale = "zh_CN.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_CN.UTF-8";
+    LC_IDENTIFICATION = "zh_CN.UTF-8";
+    LC_MEASUREMENT = "zh_CN.UTF-8";
+    LC_MONETARY = "zh_CN.UTF-8";
+    LC_NAME = "zh_CN.UTF-8";
+    LC_NUMERIC = "zh_CN.UTF-8";
+    LC_PAPER = "zh_CN.UTF-8";
+    LC_TELEPHONE = "zh_CN.UTF-8";
+    LC_TIME = "zh_CN.UTF-8";
+  };
 
   # ============================================
   # 引导与内核
