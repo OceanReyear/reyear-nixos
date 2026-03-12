772cd276edfc37d3b81bf03945b71994896cca2d
reyear <reyearocean@qq.com>
2026-03-12 17:44:14 +0800

    极简配置，未做个性化配置

--- hosts/reyear-nixos/home/zsh.nix
+++ hosts/reyear-nixos/home/zsh.nix
@@ -4,20 +4,23 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
     oh-my-zsh = {
       enable = true;
-      theme = "robbyrussell";
-      plugins = [
-        "git"
-        "zsh-autosuggestions"
-        "zsh-syntax-highlighting"
-      ];
+      theme = "powerlevel10k";
+      plugins = [ "git" ];
     };
     history = {
       size = 10000;
       save = 10000;
       share = true;
     };
+    initContent = ''
+      if [[ -f ~/.p10k.zsh ]]; then
+        source ~/.p10k.zsh
+      fi
+    '';
   };
 
   programs.fzf = {
