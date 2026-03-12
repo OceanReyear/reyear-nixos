0195a664d4ac8997c66ca10ccaf9ef4e529bf3a8
reyear <reyearocean@qq.com>
2026-03-12 17:29:00 +0800

    极简配置，未做个性化配置

--- hosts/reyear-nixos/README.md
+++
@@ -1,7 +0,0 @@
-# reyear-nixos
-
-主机配置入口：`configuration.nix`
-
-Home Manager 用户配置：`home.nix`
-
-文档与维护说明请见：`/etc/nixos/docs/README.md`
--- hosts/reyear-nixos/home/zsh.nix
+++ hosts/reyear-nixos/home/zsh.nix
@@ -1,6 +1,25 @@
 { pkgs, ... }:
 
 {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "git"
+        "zsh-autosuggestions"
+        "zsh-syntax-highlighting"
+      ];
+    };
+    history = {
+      size = 10000;
+      save = 10000;
+      share = true;
+    };
+  };
+
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
@@ -9,49 +28,5 @@
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
-  };
-
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = true;
-    settings = {
-      add_newline = false;
-      format = "$username$hostname$directory$git_branch$git_status$character";
-      directory = {
-        style = "cyan";
-        truncation_length = 8;
-        truncation_symbol = "…/";
-      };
-      git_branch = { style = "yellow"; };
-      git_status = { style = "yellow"; };
-      character = {
-        success_symbol = "[➜](green)";
-        error_symbol = "[➜](red)";
-      };
-    };
-  };
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion = {
-      enable = true;
-      highlight = "fg=8";
-    };
-    syntaxHighlighting.enable = true;
-    plugins = [
-      {
-        name = "fzf-tab";
-        src = pkgs.zsh-fzf-tab;
-      }
-    ];
-    history = {
-      size = 10000;
-      save = 10000;
-      share = true;
-    };
-    initContent = ''
-      eval "$(starship init zsh)"
-    '';
   };
 }
