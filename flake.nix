6608bfe88e154d044604245303dbe286a005b500
reyear <reyearocean@qq.com>
2026-03-13 11:57:18 +0800

    极简配置，未做个性化配置

--- flake.nix
+++ flake.nix
@@ -1,12 +1,8 @@
 {
-  description = "Ryear's NixOS Configuration with Flakes";
+  description = "reyear-nixos";
 
   inputs = {
-    # NixOS 官方源，锁定 25.11 分支
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-
-    # 或者使用 unstable 获取最新软件
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
@@ -26,8 +22,6 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      # NixOS 系统配置
-      # 文档入口：docs/README.md
       nixosConfigurations.reyear-nixos = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = { inherit inputs; };
--- hosts/reyear-nixos/home/shell.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-  programs.bash = {
-    enable = true;
-  };
-
-  home.sessionPath = [ "/etc/nixos/scripts" ];
-}
